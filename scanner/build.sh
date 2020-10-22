c75df0d463b4430e0f580f5b17043c9f703744e3
Dawid Kiciński <kicinski.dawid@gmail.com>
2020-10-22 22:43:53 +0200

    Remove cpp code from repository
    To siplify things I moved all cpp code into separate repository.
    If you're intrested check: software-scanner-native

--- scanner/build.sh
+++
@@ -1,9 +0,0 @@
-
-export ANDROID_NDK=/home/dkicinski/Android/Sdk/ndk/21.3.6528147
-
-rm -rf build
-cmake -B build -S . \
- -DANDROID=ON \
-
-cd build && make
-cd ..
--- scanner/scanner/java/scanner.i
+++
@@ -1,8 +0,0 @@
-% module scanner
-
-    %
-    {
-#include <scanner/scanner.hpp>
-        % }
-
-    % include<std_string.i> % include "scanner/scanner.hpp"
